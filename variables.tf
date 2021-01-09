7c0be4a3ee288bba4725275c0b44a5e8b7c1e862
Alejandro Paredero <paredero@gmail.com>
2021-01-09 13:15:48 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,7 +8,7 @@ variable "private_key_path" {
 
 variable "key_name" {
   description = "Desired name of AWS key pair"
-  default = "mbitawskey"
+  default = "usalawskey"
 }
 
 variable "aws_region" {
@@ -16,12 +16,12 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-# Ubuntu Precise 12.04 LTS (x64)
+# Amazon Linux 2 AMI (HVM), SSD Volume Type (64-bit x86)
 variable "aws_amis" {
   default = {
-    eu-west-1 = "ami-674cbc1e"
-    us-east-1 = "ami-1d4e7a66"
-    us-west-1 = "ami-969ab1f6"
-    us-west-2 = "ami-8803e0f0"
+    eu-west-1 = "ami-01720b5f421cf0179"
+    us-east-1 = "ami-0be2609ba883822ec"
+    us-west-1 = "ami-03130878b60947df3"
+    us-west-2 = "ami-0a36eb8fadc976275"
   }
 }
